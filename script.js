2bee961f3da1cc1579479e4a2ddd1404edb65f16
Korablik78 <tophikyla@gmail.com>
2026-05-13 17:47:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+function openModal(id){
+  document.getElementById(id).style.display = "flex";
+}
+function closeModal(id){
+  document.getElementById(id).style.display = "none";
+}
+window.onclick = function(event){
+  const whiteModal = document.getElementById('whiteModal');
+  const blueModal = document.getElementById('blueModal');
+  if(event.target === whiteModal){
+    whiteModal.style.display = "none";
+  }
+  if(event.target === blueModal){
+    blueModal.style.display = "none";
+  }
+}
